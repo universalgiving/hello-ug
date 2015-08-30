231146b8fc93239f1d754a1d5e8a103682b84bd2
Zhenzhong Xu <flyfree@gmail.com>
2015-08-30 00:04:42 -0700

    remove auto publish, add secure methods

--- hello-ug.js
+++ hello-ug.js
@@ -1,44 +1,87 @@
 Tasks = new Mongo.Collection("tasks");
 Init = new Mongo.Collection("init");
 
-if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      // Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: 1}});
-    }
-  });
 
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
-
-      // Get value from form element
-      var text = event.target.text.value;
-
-      // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.text.value = "";
-    }
-  });
-
-
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {
-        $set: {checked: ! this.checked}
-      });
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
-    }
-  });
+if (Meteor.isServer) {
+    // This code only runs on the server
+    Meteor.publish("tasks", function () {
+        return Tasks.find();
+    });
 }
+
+
+if (Meteor.isClient) {
+    // This code only runs on the client
+    Meteor.subscribe("tasks");
+
+    Template.body.helpers({
+        tasks: function () {
+            if (Session.get("hideCompleted")) {
+                // If hide completed is checked, filter tasks
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: 1}});
+            } else {
+                // Otherwise, return all of the tasks
+                return Tasks.find({}, {sort: {createdAt: 1}});
+            }
+        },
+        hideCompleted: function () {
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function () {
+            return Tasks.find({checked: {$ne: true}}).count();
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
+
+            // Get value from form element
+            var text = event.target.text.value;
+
+            // Insert a task into the collection
+            Meteor.call("addTask", text)
+
+            // Clear form
+            event.target.text.value = "";
+        },
+        "change .hide-completed input": function (event) {
+            Session.set("hideCompleted", event.target.checked);
+        }
+    });
+
+
+    Template.task.events({
+        "click .toggle-checked": function () {
+            // Set the checked property to the opposite of its current value
+            Meteor.call("setChecked", this._id, ! this.checked);
+        },
+        "click .delete": function () {
+            Meteor.call("deleteTask", this._id);
+        }
+    });
+}
+
+
+Meteor.methods({
+    addTask: function (text) {
+        // Make sure the user is logged in before inserting a task
+        if (! Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+
+        Tasks.insert({
+            text: text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    },
+    deleteTask: function (taskId) {
+        Tasks.remove(taskId);
+    },
+    setChecked: function (taskId, setChecked) {
+        Tasks.update(taskId, { $set: { checked: setChecked} });
+    }
+});
