9451168bfd7202b0c20a193d0463b09b6468ce96
Zhenzhong Xu <flyfree@gmail.com>
2015-08-30 00:20:35 -0700

    update devinitdb package

--- packages/devinitdata/devinitdata.js
+++ packages/devinitdata/devinitdata.js
@@ -2,33 +2,15 @@ if (Meteor.isServer) {
     Meteor.startup(function () {
 
         if (Init.find().count() <= 0) {
-            Tasks.insert({
-                text: "Task 0: Insert a new task on to-do list!",
-                createdAt: new Date()});
 
-            Tasks.insert({
-                text: "Task 1: Does it show up yet? Look at the header section",
-                createdAt: new Date()});
-            Tasks.insert({
-                text: "Task 2: Now let's restart your vagrant environment see if the tasks are persisted?",
-                createdAt: new Date()
-            });
+            Meteor.call("addTask", "Task 0: Insert a new task on to-do list!");
+            Meteor.call("addTask", "Task 1: Does it show up yet? Look at the header section");
+            Meteor.call("addTask", "Task 2: Now let's restart your vagrant environment see if the tasks are persisted?");
+            Meteor.call("addTask", "Task 3: vagrant destroy");
+            Meteor.call("addTask", "Task 4: vagrant up");
+            Meteor.call("addTask", "Task 5: Find a way to persist a new task!");
 
-            Tasks.insert({
-                text: "Task 3: vagrant destroy",
-                createdAt: new Date()
-            });
-
-            Tasks.insert({
-                text: "Task 4: vagrant up",
-                createdAt: new Date()
-            });
-
-            Tasks.insert({
-                text: "Task 5: Find a way to persist a new task!",
-                createdAt: new Date()
-            });
-
+            
             Init.insert({
                 init: true
             })
