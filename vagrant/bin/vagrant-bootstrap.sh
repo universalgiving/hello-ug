33a650e1fe2e397a5d6749b52d9650dd1779f8e7
Zhenzhong Xu <flyfree@gmail.com>
2015-09-06 00:19:42 -0700

    remove init in vagrant

--- vagrant/bin/vagrant-bootstrap.sh
+++ vagrant/bin/vagrant-bootstrap.sh
@@ -13,5 +13,4 @@ sudo echo "sudo mount --bind /apps/meteor/hello-ug/.meteor/local/ /vagrant/.mete
 # start meteor
 cd /vagrant
 echo "bring up meteor as user: $USER"
-sudo meteor mongo /vagrant/vagrant/data/initdb.js
 nohup sudo meteor > /dev/null 2>&1 &
